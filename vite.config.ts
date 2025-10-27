5326dfa27362137f20828eaeded9e3f18acd12c5
SamLee-dedeboy <samytleezzz@gmail.com>
2025-10-26 23:43:13 -0700

    update vite config for github page actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,9 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tsconfigPaths(), tailwindcss()],
   base: "/jt_dashboard/",
-  resolve: {
-    alias: {
-      'renderers': path.resolve(__dirname, 'src/renderers'),
-      'constants': path.resolve(__dirname, 'src/constants'),
-      'lib': path.resolve(__dirname, 'src/lib'),
-    }
-  }
 })
