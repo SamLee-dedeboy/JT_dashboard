a841e0d96393b38a7a884429279ec32a3ecd69b1
SamLee-dedeboy <samytleezzz@gmail.com>
2025-10-26 23:47:10 -0700

    fix vite config path

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tsconfigPaths(), tailwindcss()],
-  base: "/jt_dashboard/",
+  base: "/JT_dashboard/",
 })
